812c94130ea65cf0bbe03d8da8b3aed6e43de919
Nic Bonetto <nick.bonetto@gmail.com>
2025-08-13 19:01:40 -0700

    feat: create streamlit app

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit
+from PIL import Image
+import torch
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+
+MODEL_NAME = 'model/'
+processor = TrOCRProcessor.from_pretrained(MODEL_NAME)
+model = VisionEncoderDecoderModel.from_pretrained(MODEL_NAME)
+
+streamlit.title('Light OCR')
+
+uploaded_file = streamlit.file_uploader('Choose an image...', type=['png', 'jpg', 'jpeg'])
+if uploaded_file:
+    image = Image.open(uploaded_file).convert("RGB")
+    image = image.resize((384, 384))
+    streamlit.image(image, caption='Uploaded Image', use_column_width=True)
+    
+    pixel_values = processor(images=image, return_tensors='pt').pixel_values
+    output_ids = model.generate(pixel_values)
+    text = processor.batch_decode(output_ids, skip_special_tokens=True)[0]
+    
+    streamlit.subheader('Recognized Text')
+    streamlit.write(text)
+
---
+++ core/model.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import VisionEncoderDecoderModel, TrOCRProcessor
+from config import MODEL_NAME
+
+device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
+processor = TrOCRProcessor.from_pretrained(MODEL_NAME, use_fast = False)
+model = VisionEncoderDecoderModel.from_pretrained(MODEL_NAME).to(device)
+
+model.config.decoder_start_token_id = processor.tokenizer.bos_token_id
+
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+if model.config.pad_token_id is None:
+    model.config.pad_token_id = processor.tokenizer.eos_token_id
